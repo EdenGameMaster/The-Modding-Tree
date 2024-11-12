eb52d41338f942e8e4f67a95598fb3c0f4a0ba63
EdenGameMaster <2704114364@qq.com>
2024-11-12 20:16:50 +0800

    Literally nothing but a bit

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("r", {
+    name: "rank", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "R", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
@@ -8,11 +8,11 @@ addLayer("p", {
     }},
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
+    resource: "ranks", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 1, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
@@ -22,7 +22,36 @@ addLayer("p", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "r", description: "R: Reset for ranks", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
-})
+    layerShown(){return true},
+    milestones: {
+        0: {
+            requirementDescription: "1 Rank",
+            effectDescription: "Unlock a buyable and an upgrade.",
+            done() { return player.r.points.gte(1) },
+            unlocked(){return true}
+         }
+        },
+    buyables: {
+        11: {
+            cost(x) { return new Decimal(1).mul(3) },
+            title() { return "<h3>" + format(this.cost()) + " Points"},
+            display() { return "The points gain is multiplied by 1.5." },
+            canAfford() { return player[this.layer].points.gte(this.cost()) },
+            buy() {
+                player[this.layer].points = player[this.layer].points.sub(this.cost())
+                setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(1))
+            },
+            
+        },
+    },
+        upgrades: {
+            11: {
+                title: "Useless buyables",
+                description: "Get 3 free buyable. While the scale of the buyable will be 1000 instead of 3",
+                cost: new Decimal(10),
+            },
+        },
+}
+)
